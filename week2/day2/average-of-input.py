3a47e371e02170176677131477a1739a887a0357
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 15:51:42 +0100

    sum and average calculation of five numbers done

---
+++ week2/day2/average-of-input.py
@@ -0,0 +1,16 @@
+x = int(input("Please enter five numbers to summarize and average them. Enter the first number now: "))
+y = int(input("Please enter the second number now: "))
+z = int(input("Please enter the third number now: "))
+t = int(input("Please enter the fourth number now: "))
+q = int(input("Please enter the fifth number now: "))
+
+a = x + y + z + t + q
+b = a/5
+
+print("Sum: " + str(a) + ", Average: " + str(b))
+
+
+# Write a program that asks for 5 integers in a row,
+# then it should print the sum and the average of these numbers like:
+#
+# Sum: 22, Average: 4.4
