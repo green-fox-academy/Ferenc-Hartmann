1c15843a1424331a0a7948f05926bd6aa5236d62
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 14:17:21 +0100

    seconds in a day debugged and variables mutated nicely

--- week2/day2/seconds-in-a-day.py
+++ week2/day2/seconds-in-a-day.py
@@ -1,6 +1,6 @@
 Hour = 14
-Minute = 35
-Second = 22
+Minute = 34
+Second = 42
 Fulldayinsec = 24 * 60 * 60
 Remaintime = Fulldayinsec - (((Hour * 60) + Minute)*60 + Second)
 
---
+++ week2/day2/variable-mutation.py
@@ -0,0 +1,58 @@
+a = 3
+a +=10
+print(a)
+
+b = 100
+b -= 7
+print(b)
+
+c = 44
+c *= 2
+print(c)
+
+d = 125
+d //= 5
+print(d)
+
+e = 8
+e = pow(e,3)
+print(e)
+
+f1 = 123
+f2 = 345
+if f1>f2:
+    print(True)
+else:
+    print(False)
+
+g1 = 350
+g2 = 200
+if 2*g2>g1:
+    print(True)
+else:
+    print(False)
+
+h = 1357988018575474
+if h/11 == h//11:
+    print(True)
+else:
+    print(False)
+
+i1 = 10
+i2 = 3
+if i1 > i2*i2:
+    if i1 < i2*i2*i2:
+        print(True)
+    else:
+        print(False)
+else:
+    print(False)
+
+j = 1521
+if j/3 == j//3 or j/5 == j//5:
+    print(True)
+else:
+    print(False)
+
+k = "Apple" * 4
+print (k)
