53bb270019805b16427cc664b930d0d0e988b75d
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 13:07:42 +0100

    humpty-dumpty remade according to wikipedia

--- week2/day2/humpty-dumpty.py
+++ week2/day2/humpty-dumpty.py
@@ -1,4 +1,4 @@
-print("All the king's horses and all the king's men")
-print("Humpty Dumpty had a great fall.")
 print("Humpty Dumpty sat on a wall,")
+print("Humpty Dumpty had a great fall.")
+print("All the king's horses and all the king's men")
 print("Couldn't put Humpty together again.")
