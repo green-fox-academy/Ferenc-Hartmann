1d8e6506e941f09ee9c88cafd8f4a06c5af01486
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 16:09:22 +0100

    printing the bigger number form the two input solved

---
+++ week2/day2/print-bigger.py
@@ -0,0 +1,9 @@
+x = int(input("Please enter two number to decide which of them is the bigger. Please enter the first number: "))
+y = int(input("Please enter the second number: "))
+
+if x > y:
+    print(x)
+if x < y:
+    print(y)
+else:
+    print("Equal")
