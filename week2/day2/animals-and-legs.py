69106c6bd8c031a24a7f9f608b6e583eb6cf20c5
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 15:43:56 +0100

    Mile to km converter revised and a minor fix done. Farmer animall eg counter done.

---
+++ week2/day2/animals-and-legs.py
@@ -0,0 +1,6 @@
+x = int(input("Please enter the number of chickens on your farm: "))
+y = int(input("Please enter the number of pigs on your farm: "))
+
+l = 2*x + 4*y
+
+print("All of the animals on your farm has: " + str(l) + " legs")
--- week2/day2/mile-to-km-converter.py
+++ week2/day2/mile-to-km-converter.py
@@ -1,3 +1,3 @@
-x = int(input("Please enter the distance in kilometers to conert it to miles: "))
+x = int(input("Please enter the distance in kilometers to convert it to miles: "))
 y = x*1.6
 print("The distance in miles: " + str(y))
