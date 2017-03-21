a9c83e65a7c772b0d78a14a35cf42065075f7b54
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 16:05:08 +0100

    That flawless 'One two a lot' code was not flawless. Now it is.

--- week2/day2/one-two-a-lot.py
+++ week2/day2/one-two-a-lot.py
@@ -9,10 +9,3 @@ if x > 0:
         print("A lot")
 else:
     print("Not enough")
-
-
-# Write a program that reads a number form the standard input,
-# If the number is zero or smaller it should print: Not enough
-# If the number is one it should print: One
-# If the number is two it should print: Two
-# If the number is more than two it should print: A lot
