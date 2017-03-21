497273c454aabc982443fa27d6a0e8726b9e17f5
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 13:29:27 +0100

    Cuboid mssion done

---
+++ week2/day2/cuboid.py
@@ -0,0 +1,8 @@
+Aedge = 10
+Bedge = 10
+Cedge = 10
+
+Surface = 2 * (Aedge * Bedge + Aedge * Cedge + Bedge * Cedge)
+Volume = Aedge * Bedge * Cedge
+print ("Surface area: " + str(Surface))
+print ("Volume: " + str(Volume))
