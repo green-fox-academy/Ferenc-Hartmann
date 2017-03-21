5a28adb38b064f715dbfa9d3d404bf1e276fa9b2
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 12:03:34 +0100

    A fantastic logical mission is accomplished. Best prints ever in python

---
+++ week2/day2/coding-hours.py
@@ -0,0 +1,7 @@
+codehoursinaday = 6
+semester = 17
+Semestercodingtime = codehoursinaday * semester * 5
+print ("Hours spent with coding in a semester: " + str(Semestercodingtime) + "hours")
+
+semesterworkhours = semester * 52
+print ("Percentage of the coding hours in the semester: " +str(Semestercodingtime/semesterworkhours*100) + "%")
