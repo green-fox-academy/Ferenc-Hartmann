1dfced00c8e7373bcb65fd1a847cd390713dc748
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-21 16:04:09 +0100

    'One two a lot' quest solved flawlessly. Absoluetly flawless.

---
+++ week2/day2/one-two-a-lot.py
@@ -0,0 +1,18 @@
+x = int(input("Please enter a number to proceed this mission: "))
+
+if x > 0:
+    if x == 1:
+        print("One")
+    if x == 2:
+        print("Two")
+    if x > 2:
+        print("A lot")
+else:
+    print("Not enough")
+
+
+# Write a program that reads a number form the standard input,
+# If the number is zero or smaller it should print: Not enough
+# If the number is one it should print: One
+# If the number is two it should print: Two
+# If the number is more than two it should print: A lot
