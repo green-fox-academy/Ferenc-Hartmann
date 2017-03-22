d7a10ee89b5d67a6ee3b9d7e34d94d2f0d048e6b
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 12:53:53 +0100

    Code wrote to decide which list is the longer and print them all if a condition is true. V1.0, tested, works well.

---
+++ week2/day3/compare-length.py
@@ -0,0 +1,13 @@
+
+# - Create a variable named `p1`
+#   with the following content: `[1, 2, 3]`
+# - Create a variable named `p2`
+#   with the following content: `[4, 5]`
+# - Print if `p2` has more elements than `p1`
+
+p1 = [1, 2, 3]
+p2 = [4, 5]
+
+if len(p1) > len(p2):
+    print (p1)
+    print (p2)
