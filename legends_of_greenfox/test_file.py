bb2018e9a60822a9353e327704841f6d9c931531
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 22:07:57 +0100

    Battle calculation now works fine, tested.

--- legends_of_greenfox/test_file.py
+++ legends_of_greenfox/test_file.py
@@ -1,29 +1,30 @@
 import random
-health = 10
-damage = 2
+health = 20
+damage = 4
 dex = 8
-dragon_health = 10
-dragon_damage = 1
-dragon_dex = 10
+dragon_health = 20
+dragon_damage = 2
+dragon_dex = 8
 a1 = 0
 a2 = 0
 a3 = 0
 anykey = str(0)
 
-while True:
-    r = random.randrange(2, 12)
-    print(r)
-    if (dex + r) > (dragon_dex + r):
-        dragon_health -= damage
-        print("dragon took" + str(damage) + "damage")
-    if (dex + r) < (dragon_dex + r):
-        health -= dragon_damage
-        print("You took" + str(dragon_damage) + "damage")
-    else:
+while health > 0 and dragon_health > 0:
+    ua = dex + random.randrange(2, 12)
+    da = dragon_dex + random.randrange(2, 12)
+    if ua > da:
+        dragon_health = (dragon_health - damage)
+        print("dragon took " + str(damage) + " damage")
+        print("")
+        if dragon_health == 0:
+            print("dragon_health 0")
+    elif ua < da:
+        health =  (health - dragon_damage)
+        print("You took " + str(dragon_damage) + " damage")
+        print("")
+        if health == 0:
+            print("health 0")
+    elif ua == da:
         print("You attacked very hard but no damage")
-    if health == 0:
-        print("health 0")
-        break
-    if dragon_health == 0:
-        print("dragon_health 0")
-        break
+        print(ua, da)
