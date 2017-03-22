02aceebe11308b99e2b10cc80228c77ea51c37d5
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 13:17:00 +0100

    Code wrote to increment a list's third element by one. V1.0, tested, works well.

---
+++ week2/day3/increment-element.py
@@ -0,0 +1,10 @@
+
+# - Create a variable named `t`
+#   with the following content: `[1, 2, 3, 4, 5]`
+# - Increment the third element
+# - Print the third element
+
+
+t = [1, 2, 3, 4, 5]
+t[2] += 1
+print (t)
