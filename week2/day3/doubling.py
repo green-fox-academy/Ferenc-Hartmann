b02ce7101c3bcaeb2c77f9f21b6c08749b0d52ef
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 10:38:54 +0100

    doubling function work and fine

---
+++ week2/day3/doubling.py
@@ -0,0 +1,12 @@
+# - Create a variable named `ak` and assign the value `123` to it
+# - Create a function called `doubling` that doubles it's input parameter
+# - Print the result of `doubling(ak)`
+
+ak = 123
+
+
+def doubling(ak):
+    ak = ak * 2
+    print(ak)
+
+doubling(ak)
