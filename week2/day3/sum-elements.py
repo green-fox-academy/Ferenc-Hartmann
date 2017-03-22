ba86e7d90a1f4e29154d46c9273d1e7b3056dffe
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 12:59:56 +0100

    Code wrote to summarize second and third element in a list. V1.0, tested, works well.

---
+++ week2/day3/sum-elements.py
@@ -0,0 +1,9 @@
+
+# - Create a variable named `r`
+#   with the following content: `[54, 23, 66, 12]`
+# - Print the sum of the second and the third element
+
+
+r = [54, 23, 66, 12]
+p = r[1] + r[2]
+print (p)
