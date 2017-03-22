8d76d3b60d607d3145de7fe281550bf0a77e5c5f
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 13:14:40 +0100

    Code wrote to change an element of a list. V1.0, tested, works well.

---
+++ week2/day3/change-element.py
@@ -0,0 +1,10 @@
+
+# - Create an array variable named `s`
+#   with the following content: `[1, 2, 3, 8, 5, 6]`
+# - Change the 8 to 4
+# - Print the fourth element
+
+
+s = [1, 2, 3, 8, 5, 6]
+s[3] = 4
+print (s)
