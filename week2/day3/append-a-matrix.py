bf317e2c2ed6c9586bbf3ad797e6d7d7b9942089
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 14:52:22 +0100

    Added an a letter to the end of every element of a list. V1.0, tested, works well.

---
+++ week2/day3/append-a-matrix.py
@@ -0,0 +1,11 @@
+
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macs", "cic"]`
+# - Add all elements an `"a"` at the end
+
+nimals = ["kuty", "macs", "cic"]
+
+for x in range(len(nimals)):
+    nimals[x] += "a"
+
+print(nimals)
