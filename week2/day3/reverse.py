18a03267cc63ae59b2dbdddad1310b4c90932bf3
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 15:09:15 +0100

    List reversed. V1.0, tested, works well.

---
+++ week2/day3/reverse.py
@@ -0,0 +1,9 @@
+
+# - Create a variable named `aj`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Reverse the order of the elements in `aj`
+# - Print the elements of the reversed `aj`
+
+aj = [3, 4, 5, 6, 7]
+aj.reverse()
+print(aj)
