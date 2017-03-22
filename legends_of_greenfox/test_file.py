5dc51fcb6abbd29c49cbaa59e344c5b33f3940ef
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-22 17:35:00 +0100

    Legends decision making system reached level 2. Also solved the program terminating proccess.

--- legends_of_greenfox/test_file.py
+++ legends_of_greenfox/test_file.py
@@ -1,16 +1,35 @@
+hp = 0
+damage = 0
+dex = 0
 a1 = 0
-def first_decision(x):
+a2 = 0
+def first_decision():
     while True:
-        a1 = input("")
-        if str(a1) == "q" or str(a1) == "Q":
-            break
-            print("quit")
+#Main Menu
+        a1 = input("a1")
+        if str(a1) == "s" or str(a1) == "S":
+            print("start")
+            while True:
+#Character choosing
+                a2 = input("a2")
+                if str(a2) == "a" or str(a1) == "A":
+                    print("archer")
+                elif str(a2) == "k" or str(a2) == "K":
+                    print("knight")
+                elif str(a2) == "q" or str(a2) == "Q":
+                    raise SystemExit
+                    print("quit2")
+                else:
+                    print("Please press [A] or [K]")
+
+
         elif str(a1) == "h" or str(a1) == "H":
             print("help")
-        elif str(a1) == "s" or str(a1) == "S":
-            print("start")
+        elif str(a1) == "q" or str(a1) == "Q":
+            raise SystemExit
+            print("quit")
         elif str(a1) == "m" or str(a1) == "M":
             print("main")
         else:
             print("Please press [Q] or [S] or [H]")
-first_decision(x)
+first_decision()
