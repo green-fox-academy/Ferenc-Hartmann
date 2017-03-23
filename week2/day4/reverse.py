87f66a57bcf3d850dbe9621f5b6b8c68f3b73b60
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-23 11:44:52 +0100

    String reverser function created. Exercise 2 solved with this.

---
+++ week2/day4/reverse.py
@@ -0,0 +1,9 @@
+# Reverse the string
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+def reverse(n):
+    n = n [::-1]
+    return n
+
+print(reverse(reversed))
