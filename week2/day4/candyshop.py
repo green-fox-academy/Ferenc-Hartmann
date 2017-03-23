9802db26f653d076012a8e71e305287db0b4c2cb
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-23 15:29:09 +0100

    Candyshop exercise solved. Changed a list's two element successfully. Tested, works well.

---
+++ week2/day4/candyshop.py
@@ -0,0 +1,23 @@
+#import java.util.*;
+
+#public class Candyshop{
+#  public static void main(String... args){
+#    ArrayList<Object> arrayList = new ArrayList<Object>();
+#    arrayList.add("Cupcake");
+#    arrayList.add(2);
+#    arrayList.add("Brownie");
+#    arrayList.add(false);
+
+#    // Accidentally we added "2" and "false" to the list.
+#    // Your task is to change from "2" to "Croissant" and change from "false" to "Ice cream"
+#    // No, don't just remove the lines
+
+#    System.out.println(arrayList);
+# }
+#}
+
+list = ["Cupcake", 2, "Brownie", "false"]
+list[1] = "Croissant"
+list[3] = "Ice cream"
+
+print(list)
