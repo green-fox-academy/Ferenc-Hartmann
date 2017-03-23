d6574333fa87ada62537bcb5a2b2e38759578bae
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-23 12:01:24 +0100

    Exercise 4 solved by slicing the string. Tested, works well.

---
+++ week2/day4/takeslonger.py
@@ -0,0 +1,10 @@
+# When saving this quote a disk error has occured. Please fix it.
+# Add "always takes longer than" between the words "It" and "you"
+
+quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law."
+
+#print(len("Hofstadter's Law: It"))
+
+quote = (quote[0:20] + " always takes longer than " + quote[21:])
+
+print(quote)
