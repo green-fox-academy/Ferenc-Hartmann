f40acd799a9f1ac44e70aaf9d2ac9b90fca4d6ee
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-24 11:31:09 +0100

    Legends. Tes_file cloned into Legends file. Legends file copied into structure without text arts. Tested, works fine.

--- legends_of_greenfox/structure.py
+++ legends_of_greenfox/structure.py
@@ -1,3 +1,4 @@
+#Legends of Greenfox: Made by Ferenc Hartmann from Greenfox Academy
 import random
 health = 0
 damage = 0
@@ -9,108 +10,153 @@ user_input_lvl1 = 0
 user_input_lvl2 = 0
 user_input_lvl3 = 0
 anykey = str(0)
+battle_rounds = 0
 
+#Interactive program starts here
 while True:
     #Main Menu
-    user_input_lvl1 = input("user_input_lvl1")
+    user_input_lvl1 = input("")
     if str(user_input_lvl1) == "s" or str(user_input_lvl1) == "S":
         print("character choosing screen")
         while True:
-            user_input_lvl2 = input("user_input_lvl2")
+            character_choose_screen()
+            user_input_lvl2 = input("")
             #Choosing archer
             if str(user_input_lvl2) == "a" or str(user_input_lvl2) == "A":
-                print("archer chosen screen")
+                archer_chosen()
                 health = 10
                 damage = 2
                 dex = 12
-                anykey = str(input("press [Enter]"))
-                print("archer prologue screen")
+                anykey = str(input(""))
+                prologue()
                 while True:
                     #Archer story starts here
-                    user_input_lvl3 = input("user_input_lvl3")
+                    user_input_lvl3 = input("")
                     if str(user_input_lvl3) == "a" or str(user_input_lvl3) == "A":
-                        print("archer pre-battle screen")
-                        anykey = str(input("press [Enter]"))
+                        dragon_attacks()
+                        anykey = str(input(""))
+                        black_screen()
                         #Battle calculation
                         while health > 0 and dragon_health > 0:
+                            battle_rounds += 1
                             ua = dex + random.randrange(2, 12)
                             da = dragon_dex + random.randrange(2, 12)
                             if ua > da:
                                 dragon_health = (dragon_health - damage)
-                                print("dragon took " + str(damage) + " damage")
+                                print("                                                                       ---------------ROUND " + str(battle_rounds) + "---------------")
+                                print("")
+                                print("                                                                                DRAGON TOOK " + str(damage) + " DAMAGE")
+                                print("                                                        YOUR HEALTH: " + str(health) +           "                                     DRAGON'S HEALTH: " + str(dragon_health) + "  ")
                                 print("")
                                 if dragon_health == 0:
-                                    print("dragon_health 0 and you win!")
+                                    anykey = str(input(""))
+                                    wingame()
+                                    anykey = str(input(""))
+                                    quit_screen()
+                                    raise SystemExit
                             elif ua < da:
                                 health =  (health - dragon_damage)
-                                print("You took " + str(dragon_damage) + " damage")
+                                print("                                                                       ---------------ROUND " + str(battle_rounds) + "---------------")
                                 print("")
+                                print("                                                                                 YOU TOOK " + str(dragon_damage) + " DAMAGE")
+                                print("                                                        YOUR HEALTH: " + str(health) +           "                                     DRAGON'S HEALTH: " + str(dragon_health) + "  ")
+                                print("")
+
                                 if health == 0:
-                                    print("health 0 and you died")
+                                    anykey = str(input(""))
+                                    losegame()
+                                    anykey = str(input(""))
+                                    quit_screen()
+                                    raise SystemExit
                             elif ua == da:
-                                print("You attacked very hard but no damage")
+                                print("                                                                       ---------------ROUND " + str(battle_rounds) + "---------------")
+                                print("")
+                                print("                                                                         YOU ATTACK VERY HARD BUT NO DAMAGE")
                                 print("")
                     elif str(user_input_lvl3) == "r" or str(user_input_lvl3) == "R":
-                        print("game over")
+                        runlosegame()
+                        anykey = str(input(""))
+                        quit_screen()
                         raise SystemExit
                     elif str(user_input_lvl3) == "q" or str(user_input_lvl3) == "Q":
-                        print("quit3")
+                        quit_screen()
                         raise SystemExit
                     else:
                         print("Please press [A] or [R]")
             #Choosing knight
             if str(user_input_lvl2) == "k" or str(user_input_lvl2) == "K":
-                print("knight chosen screen")
+                knight_chosen()
                 health = 20
                 damage = 2
                 dex = 8
-                anykey = str(input("press [Enter]"))
-                print("knight prologue screen")
+                anykey = str(input(""))
+                prologue()
                 while True:
                     #Knight story starts here
-                    user_input_lvl3 = input("user_input_lvl3")
+                    user_input_lvl3 = input("")
                     if str(user_input_lvl3) == "a" or str(user_input_lvl3) == "A":
-                        print("knight pre-battle screen")
-                        anykey = str(input("press [Enter]"))
+                        dragon_attacks()
+                        anykey = str(input(""))
+                        black_screen()
                         #Battle calculation
                         while health > 0 and dragon_health > 0:
+                            battle_rounds += 1
                             ua = dex + random.randrange(2, 12)
                             da = dragon_dex + random.randrange(2, 12)
                             if ua > da:
                                 dragon_health = (dragon_health - damage)
-                                print("dragon took " + str(damage) + " damage")
+                                print("                                                                       ---------------ROUND " + str(battle_rounds) + "---------------")
+                                print("")
+                                print("                                                                                DRAGON TOOK " + str(damage) + " DAMAGE")
+                                print("                                                        YOUR HEALTH: " + str(health) +           "                                     DRAGON'S HEALTH: " + str(dragon_health) + "  ")
                                 print("")
                                 if dragon_health == 0:
-                                    print("dragon_health 0 and you win!")
+                                    anykey = str(input(""))
+                                    wingame()
+                                    anykey = str(input(""))
+                                    quit_screen()
+                                    raise SystemExit
                             elif ua < da:
                                 health =  (health - dragon_damage)
-                                print("You took " + str(dragon_damage) + " damage")
+                                print("                                                                       ---------------ROUND " + str(battle_rounds) + "---------------")
                                 print("")
+                                print("                                                                                 YOU TOOK " + str(dragon_damage) + " DAMAGE")
+                                print("                                                        YOUR HEALTH: " + str(health) +           "                                     DRAGON'S HEALTH: " + str(dragon_health) + "  ")
+                                print("")
+
                                 if health == 0:
-                                    print("health 0 and you died")
+                                    anykey = str(input(""))
+                                    losegame()
+                                    anykey = str(input(""))
+                                    quit_screen()
+                                    raise SystemExit
                             elif ua == da:
-                                print("You attacked very hard but no damage")
+                                print("                                                                       ---------------ROUND " + str(battle_rounds) + "---------------")
+                                print("")
+                                print("                                                                         YOU ATTACK VERY HARD BUT NO DAMAGE")
                                 print("")
                     elif str(user_input_lvl3) == "r" or str(user_input_lvl3) == "R":
-                        print("game over")
+                        runlosegame()
+                        anykey = str(input(""))
+                        quit_screen()
                         raise SystemExit
                     elif str(user_input_lvl3) == "q" or str(user_input_lvl3) == "Q":
-                        print("quit3")
+                        quit_screen()
                         raise SystemExit
                     else:
                         print("Please press [A] or [R]")
             elif str(user_input_lvl2) == "q" or str(user_input_lvl2) == "Q":
-                print("quit3")
+                quit_screen()
                 raise SystemExit
             else:
                 print("Please press [A] or [K]")
     elif str(user_input_lvl1) == "h" or str(user_input_lvl1) == "H":
-        print("help")
+        help_screen()
     elif str(user_input_lvl1) == "q" or str(user_input_lvl1) == "Q":
-        print("quit")
+        quit_screen()
         raise SystemExit
     elif str(user_input_lvl1) == "m" or str(user_input_lvl1) == "M":
-        print("main")
+        main_screen()
     else:
         print("Please press [Q] or [S] or [H]")
 
