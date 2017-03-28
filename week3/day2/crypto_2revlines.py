05c409525c34d6493ad4fe6ab47dfb74665a778b
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-28 13:20:49 +0200

    duplicated character exercise cleaned from one useless comment and the reverse lines exercise solved. Tested, works nearly flawless.

--- week3/day2/crypto_2revlines.py
+++ week3/day2/crypto_2revlines.py
@@ -1,21 +1,20 @@
-# Create a method that decrypts the texts/duplicated_chars.txt
-
+# Create a method that decrypts texts/reversed_zen_lines.txt
 #def decrypt(file_name):
 #    pass
 
 my_file = 0
 def decrypt(file_name):
-    my_file = open("duplicated-chars.txt", "r")
+    my_file = open("reversed-lines.txt", "r")
     test = my_file.readlines()
     n = 0
     for i in test:
         a = test[n]
         n += 1
-        p = len(a) // 2
+        p = (len(a))
         r = 0
         f = ""
         for j in range(p):
-            f += a[r]
-            r += 2
+            f += a[-(r+1)]
+            r += 1
         print(f)
 decrypt(my_file)
