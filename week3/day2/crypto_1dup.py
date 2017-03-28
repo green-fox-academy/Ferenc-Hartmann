a25e610c44ae297a80b582c2e29750926e846cb2
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-28 12:12:04 +0200

    Day2 exercise 1 (decrypt doubled characters) solved, was so hard but works fine.

---
+++ week3/day2/crypto_1dup.py
@@ -0,0 +1,22 @@
+# Create a method that decrypts the texts/duplicated_chars.txt
+
+#def decrypt(file_name):
+#    pass
+
+my_file = 0
+def decrypt(file_name):
+    my_file = open("duplicated-chars.txt", "r")
+    test = my_file.readlines()
+    n = 0
+    for i in test:
+        a = test[n]
+        n += 1
+        p = len(a) // 2
+        r = 0
+        f = ""
+        for j in range(p):
+            f += a[r]
+            #print(f)
+            r += 2
+        print(f)
+decrypt(my_file)
---
+++ week3/day2/testfile.py
@@ -0,0 +1,10 @@
+# Create a method that decrypts the texts/duplicated_chars.txt
+
+#def decrypt(file_name):
+#    pass
+
+
+
+a = "t"
+b = "e"
+print(a + b)
