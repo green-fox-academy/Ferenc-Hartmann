c946865102329a2fe8b001252fcfe9923ffe5ac0
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-30 13:56:37 +0200

    character changer recursively coded by a function in function

---
+++ week3/day4/string1.py
@@ -0,0 +1,16 @@
+# Given a string, compute recursively (no loops) a new string where all the
+# lowercase 'x' chars have been changed to 'y' chars.
+
+string = "Please xummi the xummi of bunnies to xummi their xummies :"
+def normalstringer(n):
+    def stringer(n):
+        stringy = ""
+        if len(stringy) == len(n): #base case
+            return ""
+        elif n[(len(n)-1)] == "x":
+            return "y" +(stringer(n[0:(len(n)-1)]))
+        else:
+            stringy +=n[(len(n)-1)]
+            return stringy +(stringer(n[0:(len(n)-1)]))
+    return stringer(n)[::-1]
+print(normalstringer(string))
---
+++ week3/day4/testfile.py
@@ -0,0 +1,17 @@
+string = "Please enter the number of bunnies to sum their ears :"
+print(string)
+print(string[0])
+stringy = ""
+stringy += string[0]
+stringy += string[1]
+a = int(len(string))
+n = 5
+print(string[0:(len(string) - n)])
+print(string[(len(string)-1)])
+print(string[0:(len(string)-1)])
+
+a = 5
+b = 4
+c = 3
+d = 2
+e = 1
