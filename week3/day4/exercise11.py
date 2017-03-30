3fa771bbc97119341cb590b9b345ad90ac6452c1
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-31 00:20:29 +0200

    Fractal practise. Hexagon coded, getting better.

--- week3/day4/exercise11.py
+++ week3/day4/exercise11.py
@@ -1,31 +1,25 @@
+import time
 from tkinter import*
 root = Tk()
 canvas = Canvas(root, width='600', height='600', bg='yellow')
 canvas.pack()
 
+def drawsquare(x, y, size):
+    canvas.create_rectangle(x, y, x+size, y+size, outline='black', width=1)
+
+def recursive_sg(x, y, size):
+    drawsquare(x, y, size)
+    time.sleep(0.1)
+    canvas.update()
+    if size > 5:
+        recursive_sg(size/3+x, y, size/3)
+        recursive_sg(x + 2/3*size, y+size/3, size/3)
+        recursive_sg(x, y + 1/3*size, size/3)
+        recursive_sg(x + 1/3*size, y + 2/3*size, size/3)
+
+recursive_sg(10, 10, 550)
 
 
-def drawline1(x, y, size):
-    canvas.create_line(x-size/3, 0, x-size/3, size, fill='blue')
-def drawline2(x, y, size):
-    canvas.create_line(x-size*2/3, 0, x-size*2/3, size, fill='blue')
-def drawline3(x, y, size):
-    canvas.create_line(x-y, y*1/3, size, y-size*2/3, fill='blue')
-def drawline4(x, y, size):
-    canvas.create_line(x-y, y*2/3, size, y-size/3, fill='blue')
-
-#drawlines(600,600,600)
-
-def recursive_lines(x, y, size):
-    drawline1(x, y, size)
-    drawline2(x, y, size)
-    drawline3(x, y, size)
-    drawline4(x, y, size)
-
-    if size > 20:
-        recursive_lines(size*1/3+x/3, y/3, size*1/3)
-
-recursive_lines(600, 600, 600)
 
 
 root.mainloop()
--- week3/day4/testfile.py
+++ week3/day4/testfile.py
@@ -1,24 +1,11 @@
 import time
 from tkinter import*
 root = Tk()
-canvas = Canvas(root, width='600', height='600', bg='yellow')
+canvas = Canvas(root, width='600', height='600', bg='white')
 canvas.pack()
 
-def drawsquare(x, y, size):
-    canvas.create_rectangle(x, y, x+size, y+size, outline='black', width=1)
-
-def recursive_sg(x, y, size):
-    drawsquare(x, y, size)
-    time.sleep(0.1)
-    canvas.update()
-    if size > 5:
-        recursive_sg(size/3+x, y, size/3)
-        recursive_sg(x + 2/3*size, y+size/3, size/3)
-        recursive_sg(x, y + 1/3*size, size/3)
-        recursive_sg(x + 1/3*size, y + 2/3*size, size/3)
-
-recursive_sg(10, 10, 550)
-
+a=canvas.create_polygon([0,300, 150,40, 450,40, 600,300, 450,560, 150,560], outline='black', width=2, fill='')
+canvas.create_oval(0, 0, 600, 600, outline='yellow', width=2)
 
 
 
