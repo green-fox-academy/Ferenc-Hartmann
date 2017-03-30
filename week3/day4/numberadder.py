512d8d48d752efc4e67b6ff0d4d8f96069d817b9
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-03-30 10:21:10 +0200

    numberadder function coded.

--- week3/day4/numberadder.py
+++ week3/day4/numberadder.py
@@ -1,13 +1,12 @@
-# Write a recursive function that takes one parameter: n and counts down from n.
+# Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
 
 number = input("Please enter a positiv number to start the count down from :" )
 
 def counter(n):
     if n == 1: #base case
-        #print(n)
         return 1
     else:
-        #print(n)
-        return (counter(int(n)-1))
+        return (int(n) + (counter(int(n)-1)))
 
 counter(number)
+print(counter(number))
