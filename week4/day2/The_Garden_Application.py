c11382043b1aeae09d9cc9c75b1aa97562af8b01
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-04 14:09:39 +0200

    flowergarden getting coded but still not ready.

--- week4/day2/The_Garden_Application.py
+++ week4/day2/The_Garden_Application.py
@@ -1,24 +1,34 @@
 class Garden():
-    name = "not set"
-    def __init__(self,water):
-        self.water = water
-        self.flower_list = []
-        self.tree_list = []
+    def __init__(self, water_volume=0):
+        self.water_volume = water_volume
+        self.flower_list = [["yellow", 0], ["blue", 0]]
+        self.tree_list = [["purple", 0], ["orange", 0]]
 
-    def watering(self, water):
+    def printer(self):
+        print("The " + str(self.flower_list[0][0]) + " flower " + "needs water.")
 
-        self.ink_amount -= a
-        return self.ink_amount
+    def add_flower(self, flower, water_volume=0):
+        self.flower_list.append(flower, water_volume)
 
-class Circle(Shape):
-    def __init__(self, x, y, width, height):
-        super().__init__(x, y)
-        self.width = width
-        self.height = height
-        print("inited")
+    def add_tree(self, tree, water_volume=0):
+        self.tree_list.append(tree, water_volume)
 
+    def watering(self, water_volume=0):
+        print("Watering with " + str(water_volume))
+        needed_water = 0
+        for flowers in range(len(self.flower_list)):
+            if self.flower_list[int(flowers)][1] < 5:
+                needed_water += 1
+        for trees in range(len(self.tree_list)):
+            if self.tree_list[int(trees)][1] < 10:
+                needed_water +=1
+        for flowers in range(len(self.flower_list)):
+            if self.flower_list[int(flowers)][1] < 5:
+                self.flower_list[int(flowers)][1] += (int(water_volume) / int(needed_water)) * 0.75
+        for trees in range(len(self.tree_list)):
+            if self.tree_list[int(trees)][1] < 10:
+                self.tree_list[int(trees)][1] += (int(water_volume) / int(needed_water)) * 0.4
 
-rect = Rectangle(1, 2, 300, 400)
-#print(rect.x, rect.width)
-#print(issubclass(Circle, Shape))
-print(time.daylight)
+supergarden = Garden()
+supergarden.printer()
+supergarden.watering(40)
