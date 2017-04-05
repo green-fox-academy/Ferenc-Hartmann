a078f33a478addb0afcf212f8b35b13c303e454e
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-05 13:40:44 +0200

    Summarize method fully tested

--- week4/day3/Ferenc_Hartmann_test.py
+++ week4/day3/Ferenc_Hartmann_test.py
@@ -1,13 +1,28 @@
 import unittest
-from Ferenc_Hartmann_work import Alma
+from Ferenc_Hartmann_work import Needtest
+from Ferenc_Hartmann_work import Summarize
 
 class Testalma(unittest.TestCase):
     def test_get_apple(self):
-        apple = Alma()
-        apple.get_apple()
+        apple = Needtest()
         self.assertEqual(apple.get_apple(), "apple")
 
+class Summar(unittest.TestCase):
+    def test_sum_zero(self):
+        numbers = Summarize()
+        self.assertEqual(numbers.sum([]), 0)
 
+    def test_sum_of_one(self):
+        numbers = Summarize()
+        self.assertEqual(numbers.sum([1]), 1)
+
+    def test_sum_of_three(self):
+        numbers = Summarize()
+        self.assertEqual(numbers.sum([1,2,2]), 5)
+
+    def test_sum_null(self):
+        numbers = Summarize()
+        self.assertEqual(numbers.sum([0]), 0)
 
 if __name__ == '__main__':
     unittest.main()
--- week4/day3/Ferenc_Hartmann_work.py
+++ week4/day3/Ferenc_Hartmann_work.py
@@ -1,3 +1,17 @@
-class Alma():
+class Needtest():
     def get_apple(Self):
         return "apple"
+
+class Summarize():
+    def sum(Self, a=[0]):
+        summed = 0
+        for i in range(len(a)):
+            summed += a[i]
+        return summed
+
+class Anagram():
+    def sum(Self, a=[0]):
+        summed = 0
+        for i in range(len(a)):
+            summed += a[i]
+        return summed
