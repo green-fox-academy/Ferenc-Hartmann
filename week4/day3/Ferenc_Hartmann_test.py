216eed8dce28453ca0596e782376b7717e701bf4
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-05 14:31:38 +0200

    Anagram wroted and tested.

--- week4/day3/Ferenc_Hartmann_test.py
+++ week4/day3/Ferenc_Hartmann_test.py
@@ -1,13 +1,14 @@
 import unittest
 from Ferenc_Hartmann_work import Needtest
 from Ferenc_Hartmann_work import Summarize
+from Ferenc_Hartmann_work import Anagram
 
-class Testalma(unittest.TestCase):
+class Alma_tester(unittest.TestCase):
     def test_get_apple(self):
         apple = Needtest()
         self.assertEqual(apple.get_apple(), "apple")
 
-class Summar(unittest.TestCase):
+class Sum_tester(unittest.TestCase):
     def test_sum_zero(self):
         numbers = Summarize()
         self.assertEqual(numbers.sum([]), 0)
@@ -23,6 +24,14 @@ class Summar(unittest.TestCase):
     def test_sum_null(self):
         numbers = Summarize()
         self.assertEqual(numbers.sum([0]), 0)
+
+class Anagramtester(unittest.TestCase):
+    def test_true(self):
+        string = Anagram()
+        self.assertEqual(string.anag("caaaica", "acicaaa"), True)
+    def test_false(self):
+        string = Anagram()
+        self.assertEqual(string.anag("cica", "acitaaaa"), False)
 
 if __name__ == '__main__':
     unittest.main()
--- week4/day3/Ferenc_Hartmann_work.py
+++ week4/day3/Ferenc_Hartmann_work.py
@@ -10,8 +10,26 @@ class Summarize():
         return summed
 
 class Anagram():
-    def sum(Self, a=[0]):
-        summed = 0
-        for i in range(len(a)):
-            summed += a[i]
-        return summed
+    def anag(self, a="", b=""):
+        a_letters = []
+        b_letters = []
+        x = 0
+        y = 0
+        for i in a:
+            if a[int(x)] == " ":
+                pass
+            else:
+                a_letters.append(a[int(x)])
+            x +=1
+        a_letters = sorted(a_letters)
+        for j in b:
+            if b[int(y)] == " ":
+                pass
+            else:
+                b_letters.append(b[int(y)])
+            y +=1
+        b_letters = sorted(b_letters)
+        if a_letters == b_letters:
+            return True
+        else:
+            return False
