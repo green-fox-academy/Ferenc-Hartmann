312bf801fc73b561e63def988e1abee3b6a21302
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-07 05:17:28 +0200

    Dune2_blitz. Game menu started.

--- My_projects/DuneII_Blitz/menusystem_class.py
+++ My_projects/DuneII_Blitz/menusystem_class.py
@@ -20,6 +20,7 @@ class Dune2_blitz():
         self.i = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\hart2.png")
         self.j = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\hart1.png")
         self.main_menu = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune1pro.png")
+        self.game_bg = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune2\game_bg.png")
 
         self.start_new_game_button = Button(width=20, height=1, fg="white", bg="black", text="Start New Game", font=("Harrington",30,"bold"), command = self.start_new_game_button_clicked)
         self.load_saved_game_button = Button(width=20, height=1, fg="white", bg="black", text="Load Saved Game", font=("Harrington",30,"bold"))
@@ -34,6 +35,26 @@ class Dune2_blitz():
         self.house_atreides_pic = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune2\houseatreides.png")
         self.house_ordos_pic = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune2\houseordos.png")
         self.house_harkonnen_pic = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune2\househarkonnen.png")
+
+        self.level1_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 1", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level2_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 2", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level3_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 3", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level4_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 4", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level5_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 5", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level6_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 6", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level7_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 7", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level8_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 8", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level9_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 9", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level10_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 10", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level11_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 11", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level12_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 12", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level13_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 13", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level14_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 14", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+        self.level15_button = Button(width=4, height=1, fg="white", bg="black", text="lvl 15", font=("Harrington",25,"bold"), command=self.back_button_clicked)
+
+        self.combat_tank_im = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune2\combat_tank.png")
+        self.siege_tank_im = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune2\siege_tank.png")
+        self.rocket_launcher_im = PhotoImage(file=r"C:\Greenfox\Ferenc-Hartmann\My_projects\DuneII_Blitz\dune2\rocket_launcher.png")
 
         self.loadscreens = 0
         self.start_button_int = 0
@@ -104,7 +125,6 @@ class Dune2_blitz():
             self.house_harkonnen_button.place(x=1156, rely=0.75, anchor=CENTER)
             self.back_button.place(x=110, rely=0.92, anchor=CENTER)
             canvas.update()
-        print(self.self.start_button_int)
 
     def game(self):
         if self.game_int > 0:
@@ -114,14 +134,38 @@ class Dune2_blitz():
             self.house_ordos_button.place(x=694, y=4000, anchor=CENTER)
             self.house_harkonnen_button.place(x=1156, y=4000, anchor=CENTER)
             self.back_button.place(x=110, y=4000, anchor=CENTER)
-        print(self.game_int)
+
+            self.item = canvas.create_image(683, 384, image=self.game_bg)
+
+            self.level1_button.place(x=200, rely=0.2, anchor=CENTER)
+            self.level2_button.place(x=300, rely=0.2, anchor=CENTER)
+            self.level3_button.place(x=400, rely=0.2, anchor=CENTER)
+            self.level4_button.place(x=500, rely=0.2, anchor=CENTER)
+            self.level5_button.place(x=600, rely=0.2, anchor=CENTER)
+
+            self.level6_button.place(x=200, rely=0.33, anchor=CENTER)
+            self.level7_button.place(x=300, rely=0.33, anchor=CENTER)
+            self.level8_button.place(x=400, rely=0.33, anchor=CENTER)
+            self.level9_button.place(x=500, rely=0.33, anchor=CENTER)
+            self.level10_button.place(x=600, rely=0.33, anchor=CENTER)
+
+            self.level11_button.place(x=200, rely=0.46, anchor=CENTER)
+            self.level12_button.place(x=300, rely=0.46, anchor=CENTER)
+            self.level13_button.place(x=400, rely=0.46, anchor=CENTER)
+            self.level14_button.place(x=500, rely=0.46, anchor=CENTER)
+            self.level15_button.place(x=600, rely=0.46, anchor=CENTER)
+
+            black_box = canvas.create_rectangle(810, 390, 1270, 710, fill="black", outline="grey", width=2)
+            self.cti = canvas.create_image(900, 450, image=self.combat_tank_im)
+            self.sti = canvas.create_image(900, 550, image=self.siege_tank_im)
+            self.rli = canvas.create_image(900, 650, image=self.rocket_launcher_im)
 
 
 
 
 dune = Dune2_blitz()
 time.sleep(0.5)
-dune.loadscreen()
+#dune.loadscreen()
 canvas.delete("all")
 dune.soundplayer()
 canvas.delete("all")
