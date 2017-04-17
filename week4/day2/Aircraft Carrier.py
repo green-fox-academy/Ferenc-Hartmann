e830991e3d58c7131dc4842a007944f0be46b36a
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-04-17 16:32:54 +0200

    Aircraft. Brand new skill learned and used: Class instance and data return without returning.

--- week4/day2/Aircraft Carrier.py
+++ week4/day2/Aircraft Carrier.py
@@ -1,11 +1,12 @@
 class Aircraft():
     def __init__(self, airplane):
-        if airplane == "F16":
-            self.F16 = ["F16", 8, 0, 30]    #[type, max_ammo, stored_ammo, basedamage]
-            return self.F16
-        if airplane == "F35":
-            self.F35 = ["F35", 12, 0, 50]
-            return self.F35
+        self.airplane = [airplane, 8, 0, 30]
+#        if airplane == "F16":
+#            self.F16 = ["F16", 8, 0, 30]    #[type, max_ammo, stored_ammo, basedamage]
+#            return self.F16
+#        if airplane == "F35":
+#            self.F35 = ["F35", 12, 0, 50]
+#            return self.F35
     def fight(self):
         damagef16 = self.F16[2] * self.F16[3]
         damagef35 = self.F35[2] * self.F35[3]
@@ -29,11 +30,8 @@ class Aircraft():
         if self.F35[0] == "F35":
             return self.F35[0]
 
-    def get_status(self):
-        if self.F16[0] == "F16":
-            return ("Type " + str(self.F16[0]) + ", Ammo: " + str(self.F16[2]) + " Base Damage: " + str(self.F16[3]) + ", All damage: " + str((self.F16[2] * self.F16[3])))
-        if self.F35[0] == "F35":
-            return ("Type " + str(self.F35[0]) + ", Ammo: " + str(self.F35[2]) + " Base Damage: " + str(self.F35[3]) + ", All damage: " + str((self.F35[2] * self.F35[3])))
+    def get_status(self, plane_type, stored_ammo, base_damage):
+        return ("Type " + str(plane_type) + ", Ammo: " + str(stored_ammo) + " Base Damage: " + str(base_damage) + ", All damage: " + str((stored_ammo * base_damage)))
 
 class Carrier():
     def __init__(self, maxammo, ammo, health):
@@ -43,27 +41,60 @@ class Carrier():
         self.plane = []
 
     def add_aircraft(self, airplane):
-        if airplane == "F16":
-            self.plane.append(Aircraft.__init__(self, airplane))
-        if airplane == "F35":
-            self.plane.append(Aircraft.__init__(self, airplane))
-
+        #if airplane == "F16":
+            #self.plane.append(Aircraft.__init__(self, airplane))
+        self.plane.append(Aircraft(airplane).airplane)
+        #if airplane == "F35":
+            #self.plane.append(Aircraft.__init__(self, airplane))
+        #    self.plane.append(Aircraft("F35"))
+        #print(self.plane)
     def fill(self):
-        sorted_planes = sorted(self.plane)
-        self.plane = sorted_planes[::-1]
+        if self.ammo > 0:
+            sorted_planes = sorted(self.plane)
+            self.plane = sorted_planes[::-1]
+            for i in range(len(self.plane)):
+                return_list = Aircraft.refill(self, self.plane[int(i)][1], self.plane[int(i)][2], self.ammo)
+                self.plane[int(i)][2] = return_list[0]
+                self.ammo = return_list[1]
+        if self.ammo == 0:
+            print("No ammo in carrier.")
+
+    def fight(self, health):
+        self.enemy_health = health
+        total_damage = 0
         for i in range(len(self.plane)):
-            return_list = Aircraft.refill(self, self.plane[int(i)][1], self.plane[int(i)][2], self.ammo)
-            self.plane[int(i)][2] = return_list[0]
-            self.ammo = return_list[1]
+            total_damage += self.plane[int(i)][2] * self.plane[int(i)][3]
+            self.plane[int(i)][2] = 0
+        self.enemy_health -= total_damage
+        if self.enemy_health <= 0:
+            print("It's dead Jim :(")
 
-water = Carrier(1000, 30, 5000)
+    def get_status(self):
+        total_damage2 = 0
+        for i in range(len(self.plane)):
+            total_damage2 += self.plane[int(i)][2] * self.plane[int(i)][3]
+        print("Aircraft count: " + str(len(self.plane)) + ", Ammo Storage: " + str(self.ammo) + ", Total damage: " + str(total_damage2))
+        print("Aircrafts:")
+        for i in range(len(self.plane)):
+            print(Aircraft.get_status(self, self.plane[int(i)][0], self.plane[int(i)][2], self.plane[int(i)][3]))
+
+water = Carrier(2000, 2000, 5000)
 water.add_aircraft("F35")
 water.add_aircraft("F16")
 water.add_aircraft("F16")
 water.add_aircraft("F35")
 water.add_aircraft("F35")
 
-
-
+water.get_status()
 water.fill()
-print(water.plane)
+water.get_status()
+
+water.fight(5000)
+water.get_status()
+water.fill()
+water.get_status()
+#print(water.plane)
+#water.fill()
+#water.fight(720)
+#print(water.enemy_health)
+#print(water.ammo)
