5633e2969f2c513e1f1932d2bf9f8ca623224f58
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-02 13:40:09 +0200

    reminder exercise solved.

---
+++ week8/day2/reminder.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 mutiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the reminder of 22 divided by 13
+var a = 13;
+var b = 22;
+
+console.log(a+b);
+console.log(b-a);
+console.log(b*a);
+console.log(b/a);
+console.log(b%a);
