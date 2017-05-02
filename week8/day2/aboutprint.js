9a476b0f18e3f4e6a3d0f747cdbcf75156a38e2c
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-02 13:37:17 +0200

    aboutmeprinter exercise solved

---
+++ week8/day2/aboutprint.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line.
+//  - Your name
+//  - Your age
+//  - Your height in meters (as a decimal fraction)
+//
+//  Example output:
+//  John Doe
+//  31
+//  1.87
+console.log('Feri');
+console.log('33');
+console.log('1.85');
