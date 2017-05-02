998e4ddf4cb7e50629655fd7b537adb2a2353567
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-02 14:02:52 +0200

    define-basic-info exercise solved.

---
+++ week8/day2/define-basic-info.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+var name = 'Feri';
+var age = 33;
+var height = 1.85;
+var married = false;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(married);
