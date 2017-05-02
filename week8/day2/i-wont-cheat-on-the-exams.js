62acb822405568ab30677375a7f75f2a5b725cc9
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-02 15:03:27 +0200

    i wont cheat on exam exercise coded.

---
+++ week8/day2/i-wont-cheat-on-the-exams.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (var j = 0; j < 100; j++) {
+  console.log('I won\'t cheat on the exam!'); 
+}
