f31816947f4c4259eee49889404e9e35d2de2452
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-02 15:20:58 +0200

    draw-triangle xercise solved.

---
+++ week8/day2/draw-triangle.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var lineCount = 4;
+
+// Write a program that draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+var star = '*'
+
+for (var i = 0; i < 4; i++) {
+  console.log(star);
+  star += '*'
+}
