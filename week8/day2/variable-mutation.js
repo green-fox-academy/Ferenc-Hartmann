6fbfd142e029bbb11882a3698375fdbd0367753a
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-02 14:31:44 +0200

    variable-mutation exercise solved.

---
+++ week8/day2/variable-mutation.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var a = 3;
+// make it bigger by 10
+var a = a + 10;
+
+
+console.log(a);
+
+
+
+
+var b = 100;
+// make it smaller by 7
+var b = b - 7;
+
+
+console.log(b);
+
+
+
+
+var c = 44;
+// double c's value
+var c = c * 2;
+
+
+console.log(c);
+
+
+
+
+var d = 125;
+// divide d's value by 5
+var d = d / 5;
+
+
+console.log(d);
+
+
+
+
+var e = 8;
+// what's the cube of e's value?
+var e = e * e * e;
+
+
+console.log(e);
+
+
+
+
+var f1 = 123;
+var f2 = 345;
+// tell if f1 is bigger than f2 (as a boolean)
+if (f1 > f2) {
+  console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+
+var g1 = 350;
+var g2 = 200;
+// tell if the double of g2 is bigger than g1 (pras a boolean)
+if ((g2 * 2) > g2) {
+  console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+
+var h = 1357988018575474;
+// tell if h has 11 as a divisor (as a boolean)
+if (h % 11 === 0) {
+  console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+
+
+var i1 = 10;
+var i2 = 3;
+// tell if i1 is higher than i2 squared and smaller than i2 cubed (as a boolean)
+if (i1 > (i2 * i2) && i1 < (i2 * i2 * i2)) {
+  console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+
+var j = 1521;
+// tell if j is dividable by 3 or 5 (as a boolean)
+if (j % 3 === 0 || j % 5 === 0) {
+  console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
+
+var k = 'Apple';
+// fill the k variable with its content 4 times
+var k = k + k + k + k;
+
+
+console.log(k);
