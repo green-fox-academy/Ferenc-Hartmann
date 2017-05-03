2e6f0748a8ed20a7b32094239b337200862aeb0d
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-03 11:58:10 +0200

    compare-kength, sum-elements, swap-elements exercises solved

---
+++ week8/day3/swap-elements.js
@@ -0,0 +1,9 @@
+'use strict';
+// - Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// - Swap the first and the third element of `abc`
+
+var abc = ["Arthur", "Boe", "Chloe"];
+
+var cba = [abc[2], abc[1], abc[0]];
+
+console.log(cba);
