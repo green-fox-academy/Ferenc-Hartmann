2700c3444e18597298417be6c369b25489c227d6
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-03 11:23:17 +0200

    greet exercise solved

---
+++ week8/day3/greet.js
@@ -0,0 +1,14 @@
+'use strict';
+// - Create variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings, dear Greenfox`
+//     - Prepare for the special case when no parameters are given
+// - Greet `al`
+
+var al = 'Greenfox';
+
+function greet(bambi = 'Master') {
+    var greeting = 'Greetings, dear ' + bambi;
+    return greeting;
+}
+console.log(greet(al));
