3d500b9aa62fa8a63ddfd64855420edd3b9d71ff
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-03 15:31:53 +0200

    reverse2 exercise solved

---
+++ week8/day3/reverse2.js
@@ -0,0 +1,13 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+var reverselist = [];
+
+for (var i = (reversed.length-1); i >= 0 ; i--) {
+    reverselist.push(reversed[i]);
+}
+
+reverselist = (reverselist.join(''));
+
+console.log(reverselist);
