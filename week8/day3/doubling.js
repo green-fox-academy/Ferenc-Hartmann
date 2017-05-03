dc06f5776deb95da3dea13899416d19c1f88ca97
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-03 11:18:51 +0200

    doubling exercise coded.

---
+++ week8/day3/doubling.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Log the result of `doubling(ak)` to the console
+
+var ak = 123;
+
+function doubling(ak) {
+    var double = ak * 2;
+    return double;
+}
+console.log(doubling(ak));
