4a362f11f8064439d25da169ed80f0a2ac305b60
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-05 16:05:30 +0200

    demo and gallery ready

---
+++ week8/day5/imgdata.js
@@ -0,0 +1,10 @@
+var displaygrabber = [
+    ['background-image: url(images/1.jpg);', 'Forest Coding', 'At Greenfox Academy we like to code in the forest. The pure nature simply give us such a creative thinking. Our mind simply speeds up like a running fox.'],
+    ['background-image: url(images/2.jpg);', 'Mountain Coding', 'At Greenfox Academy we like to code in the mountains. The pure air simply give us such a creative thinking. Our mind simply speeds up like a running fox.'],
+    ['background-image: url(images/3.jpg);', 'Deers and Coding', 'At Greenfox Academy we like to code with deers. Deers simply give us such a creative thinking. Our mind simply speeds up like a running fox.'],
+    ['background-image: url(images/4.jpg);', 'Rafting and Coding', 'At Greenfox Academy we like to code while rafting. The pure water simply give us such a creative thinking. Our mind simply speeds up like a running fox.'],
+    ['background-image: url(images/5.jpg);', 'Mountain Coding 2', 'At Greenfox Academy we like to code in the mountains. The pure air simply give us such a creative thinking. Our mind simply speeds up like a running fox.'],
+    ['background-image: url(images/6.jpg);', 'Cloud Coding', 'At Greenfox Academy we like to code in the clouds. The pure air simply give us such a creative thinking. Our mind simply speeds up like a running fox.'],
+    ['background-image: url(images/7.jpg);', 'Hard Rock Coding', 'At Greenfox Academy we like to code on hard rocks. The hard rock simply give us such a creative thinking. Our mind simply speeds up like a running fox.'],
+    ['background-image: url(images/8.jpg);', 'Northern Lights Coding', 'At Greenfox Academy we like to code under the Northern Lights. The colors simply give us such a creative thinking. Our mind simply speeds up like a running fox.']
+];
