bbc662752e1e1286b2d383dd70b70543e0114a1e
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-08 17:17:35 +0200

    shudoku general length solved

--- week9/day1/index.js
+++ week9/day1/index.js
@@ -1,7 +1,7 @@
 'use strict'
 var test = require('tape');
 
-var arraytest = [0,0,0,4,5,6,7,8,9];
+var arraytest = [[5,3,4],[6,7,2],[1,9,8]];
 // [9,0,0,4,5,6,7,8,9] => Should fail
 // [1,2,3,4,5,6,7,8,9] => ok
 // [2,1,3,4,5,6,7,8,9] => ok
@@ -44,7 +44,7 @@ function validate(array) {
         }
     } else {
         return false;
-    };
+    }
 }
 
 test('validate test', function (t) {
@@ -71,5 +71,111 @@ test('multi check', function (t) {
 
 test('negative check', function (t) {
     t.equal(validate([0,0,0,1,-4,5,7,8,9]), false);
+    t.end();
+})
+
+
+// n length sudoku *************************************************
+
+
+function shudokuN(array, elements) {
+    if (array === undefined) {
+        return false;
+    }
+    var lengthCheck = array.length;
+    if (lengthCheck === elements) {
+        var typecheck = 0;
+        for (var i = 0; i < (array.length); i++) {
+            if (typeof array[i] === 'number') {
+                typecheck++;
+            }
+        }
+        if (typecheck === elements) {
+            for (var digit = 1; digit < 9; digit++) {
+                var multiCheck = 0;
+                for (var element = 0; element < array.length; element++) {
+                    if (digit === array[element]) {
+                        multiCheck ++;
+                        if (multiCheck > 1) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            element = 0;
+            for (element = 0; element < (array.length); element++) {
+                if (array[element] < 0) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+test('element number check', function (t) {
+    t.equal(shudokuN([1,2,4,5], 4), true);
+    t.equal(shudokuN([1,2,4,5], 3), false);
+    t.equal(shudokuN([1,2,4,5], 6), false);
+    t.end();
+})
+
+
+// n length 2 dimension sudoku*************************************
+
+
+function shudokudimension(array, elements) {
+    if (array === undefined) {
+        return false;
+    }
+    if (array.length !=== elements) {
+        return false;
+    }
+
+    // for (var dimension = 0; dimension < (array.length); dimension++) {
+
+
+        var lengthCheck = array.length;
+        if (lengthCheck === elements) {
+            var typecheck = 0;
+            for (var i = 0; i < (array.length); i++) {
+                if (typeof array[i] === 'number') {
+                    typecheck++;
+                }
+            }
+            if (typecheck === elements) {
+                for (var digit = 1; digit < 9; digit++) {
+                    var multiCheck = 0;
+                    for (var element = 0; element < array.length; element++) {
+                        if (digit === array[element]) {
+                            multiCheck ++;
+                            if (multiCheck > 1) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                element = 0;
+                for (element = 0; element < (array.length); element++) {
+                    if (array[element] < 0) {
+                        return false;
+                    }
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}
+
+test('dimension check', function (t) {
+    t.equal(shudokudimension([[5,3,4],[6,7,2],[1,9,8]], 3), true);
     t.end();
 })
