463e86f9bb37b6ac9d747f3962ce73eb371e755b
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-09 15:21:44 +0200

    animalfarm exercise coded

---
+++ week9/day2/animal.farm.js
@@ -0,0 +1,78 @@
+'use strict'
+
+// Create an Animal constructor
+
+// Every animal has a hunger value, which is a number
+// Every animal has a thirst value, which is a number
+// when creating a new animal object these values are created with the default 50 value
+// Every animal can eat() which decreases their hunger by one
+// Every animal can drink() which decreases their thirst by one
+// Every animal can play() which increases both by one
+
+// Create a Farm constructor
+//
+// Every farm has list of Animals
+// Every farm has slots which defines the number of free places for animals
+// Every farm can breed() which creates a new animal if there's place for it
+// Every farm can slaughter() which removes the least hungry animal
+
+
+function animal() {
+    this.hunger = 50,
+    this.thirst = 50,
+    this.eat = function () {
+      this.hunger -= 1;
+    }
+    this.drink = function () {
+      this.thirst -= 1;
+    }
+    this.play = function () {
+      this.hunger += 1;
+      this.thirst += 1;
+    }
+}
+
+var farm = {
+    places: 5,
+    animalList: [],
+    breed: function () {
+        let a = new animal()
+        if (this.animalList.length < this.places) {
+            this.animalList.push(a);
+        };
+    },
+    slaughter: function () {
+        var hungerList = [];
+        this.animalList.forEach(function(element) {
+            hungerList.push(element.hunger);
+        });
+        for (var i = 0; i < this.animalList.length; i++) {
+            if (this.animalList[i].hunger === Math.min(...hungerList)) {
+                var leastHungry = i;
+            };
+        };
+        this.animalList.splice(leastHungry, 1);
+    },
+};
+
+
+
+farm.breed()
+farm.breed()
+farm.breed()
+farm.breed()
+farm.breed()
+farm.breed()
+farm.breed()
+farm.breed()
+farm.animalList[2].eat();
+farm.animalList[0].eat();
+farm.animalList[1].eat();
+farm.animalList[1].eat();
+farm.slaughter()
+farm.slaughter()
+
+//
+// console.log(farm.animalList[0].hunger);
+
+console.log(farm);
