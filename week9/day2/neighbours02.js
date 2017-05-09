9b0b25597b1af5bfa71bb179b51761a75fb6e004
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-09 11:29:31 +0200

    neighbours2 exercise coded

---
+++ week9/day2/neighbours02.js
@@ -0,0 +1,18 @@
+'use strict';
+// Feri and his friend drinks two beers in a round. They order three times. How many beer do they drink?
+// This code isn't working properly. The function should print: 'The result is 6'
+// Could you find the appropriate types of the variables?
+// Where can you invocate the function?
+
+
+const person = 2;
+
+function amountOfBeer(person) {
+  let i = person;
+
+  for (let i = 0; i <= 4; i++) {
+    var j = i;
+  }
+  console.log('The result is:', i + j);
+}
+amountOfBeer(person);
