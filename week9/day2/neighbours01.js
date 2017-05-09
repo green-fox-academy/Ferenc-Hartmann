efd019373bdaffab16f5e2a8ff7b9f88a8bc318e
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-09 11:19:13 +0200

    lightning talk and neighbours1 exercise coded

---
+++ week9/day2/neighbours01.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Ferenc Vágási is a good working man. He helps for his friend Góliát to carrying furniture.
+// Ferenc's wife Jutka wants to know where is his husband. She calls the company.
+// They tell her Feri has finished today he is drinking at the next pub.
+// Make the code true. Góliát is working now.
+
+const isWorking = true;
+var worker = 'Feri';
+
+if (isWorking) {
+  worker = 'Goliat';
+}
+
+console.log('Who is working now?', worker);
