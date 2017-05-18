0d330945d1e6a6914d7a1427b66b3791321fa0a9
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-18 12:59:50 +0200

    bookstore table coded

--- week10/day4/bookstore.js
+++ week10/day4/bookstore.js
@@ -18,19 +18,22 @@ var conn = mysql.createConnection({
 //     sql: "SELECT * FROM author WHERE country = ? AND home_city = ?",
 //     values: [country, city]
 // }
+// SELECT book_name, aut_name FROM book_mast JOIN author ON aut_id = book_mast.aut_id
+
 
 app.get('/', function(req, res) {
-    var result = [];
-  conn.query("SELECT book_name FROM book_mast", function(err,rows){
+  conn.query("SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast LEFT JOIN author ON book_mast.aut_id = author.aut_id LEFT JOIN category ON book_mast.cate_id = category.cate_id LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id", function(err,rows){
       if(err){
         console.log("Error connecting to Db");
+        console.log(err);
   }else {
         console.log("Data received from Db:\n");
-        var html = '<ul>';
+        var html = '<table>' + '<tr><th>book title</th>' + '<th>authors name</th>' + '<th>category</th>' + '<th>publishers name</th>' + '<th>price (USD)</th></tr>';
         for (var i = 0; i < rows.length; i++) {
-            html = html + '<li>' + (rows[i].book_name) + '</li>';
+            html = html + '<tr><td>' + (rows[i].book_name) + '</td><td>' + (rows[i].aut_name) + '</td><td>' + (rows[i].cate_descrip) + '</td><td>' + (rows[i].pub_name) + '</td><td>' + (rows[i].book_price) + '</td></tr>';
         }
-        html = html + '</ul>';
+        html = html + '</table>';
+        console.log(rows);
   }
   res.send(html);
   });
