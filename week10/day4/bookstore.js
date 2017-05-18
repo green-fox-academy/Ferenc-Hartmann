61ae61a065c3c74c28227ee58a736514f04538f1
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-18 16:49:54 +0200

    bookstore exercise coded, filter works too

--- week10/day4/bookstore.js
+++ week10/day4/bookstore.js
@@ -20,9 +20,35 @@ var conn = mysql.createConnection({
 // }
 // SELECT book_name, aut_name FROM book_mast JOIN author ON aut_id = book_mast.aut_id
 
-
 app.get('/', function(req, res) {
-  conn.query("SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast LEFT JOIN author ON book_mast.aut_id = author.aut_id LEFT JOIN category ON book_mast.cate_id = category.cate_id LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id", function(err,rows){
+    var category = '';
+    var publisher = '';
+    var plt = '';
+    var pgt = '';
+
+    if (req.query.category !== undefined) {
+        category = 'category.cate_descrip = ' + '"' + req.query.category + '" AND ';
+    }
+    if (req.query.publisher !== undefined) {
+        publisher = 'pub_name = ' + '"' + req.query.publisher + '" AND ';
+    }
+    if (req.query.plt !== undefined) {
+        plt = 'book_price < ' + '"' + req.query.plt + '" AND ';
+    }
+    if (req.query.pgt !== undefined) {
+        pgt = 'book_price > ' + '"' + req.query.pgt + '" AND ';
+    }
+
+    if (category === '' && publisher === '' && plt === '' && pgt === '') {
+        var filter = '';
+    }else {
+        filter = ' WHERE ' + category + publisher + plt + pgt;
+        filter = filter.slice(0, -5);
+    }
+
+    var query = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast LEFT JOIN author ON book_mast.aut_id = author.aut_id LEFT JOIN category ON book_mast.cate_id = category.cate_id LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id' + filter;
+    console.log (query);
+  conn.query(query, function(err,rows){
       if(err){
         console.log("Error connecting to Db");
         console.log(err);
@@ -33,7 +59,6 @@ app.get('/', function(req, res) {
             html = html + '<tr><td>' + (rows[i].book_name) + '</td><td>' + (rows[i].aut_name) + '</td><td>' + (rows[i].cate_descrip) + '</td><td>' + (rows[i].pub_name) + '</td><td>' + (rows[i].book_price) + '</td></tr>';
         }
         html = html + '</table>';
-        console.log(rows);
   }
   res.send(html);
   });
