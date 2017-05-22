a85caa90748f8302dcba9cb9edcec55a0231764f
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-22 16:50:25 +0200

    tennisgame1: firstswitch replaced

--- week11/day1/TennisGame1.js
+++ week11/day1/TennisGame1.js
@@ -1,43 +1,23 @@
 var TennisGame1 = function() {
     this.scorePlayer1 = 0;
     this.scorePlayer2 = 0;
-    this.wonPoint = function(playerName) {
-        if (playerName === "player1") {
-            this.scorePlayer1 += 1;
-        }
-        else {
-            this.scorePlayer2 += 1;
-        }
-    };
 };
 
-// TennisGame1.prototype.wonPoint = function(playerName) {
-//     if (playerName === "player1") {
-//         this.scorePlayer1 += 1;
-//     }
-//     else {
-//         this.scorePlayer2 += 1;
-//     }
-// };
+TennisGame1.prototype.wonPoint = function(playerName) {
+    if (playerName === "player1") {
+        this.scorePlayer1 += 1;
+    } else {
+        this.scorePlayer2 += 1;
+    }
+};
 
 TennisGame1.prototype.getScore = function() {
     var scoreBoard = "";
     var tempScore = 0;
     if (this.scorePlayer1 === this.scorePlayer2) {
-        switch (this.scorePlayer1) {
-            case 0:
-                scoreBoard = "Love-All";
-                break;
-            case 1:
-                scoreBoard = "Fifteen-All";
-                break;
-            case 2:
-                scoreBoard = "Thirty-All";
-                break;
-            default:
-                scoreBoard = "Deuce";
-                break;
-        }
+        var options = ["Love-All", "Fifteen-All", "Thirty-All"];
+        scoreBoard = (this.scorePlayer1 < 3) ? options[this.scorePlayer1] : "Deuce";
+
     } else if (this.scorePlayer1 >= 4 || this.scorePlayer2 >= 4) {
         var minusResult = this.scorePlayer1 - this.scorePlayer2;
         if (minusResult === 1) {
