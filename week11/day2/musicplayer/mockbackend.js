cd08cfaf17fb94884824b04590e7b2a95c7d76ff
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-24 13:09:36 +0200

    file structure modified due to solve the local host html loader in backup

---
+++ week11/day2/musicplayer/mockbackend.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/style', express.static('style'))
+
+app.get('/playlists', function(req, res) {
+    var playlists = [
+	{ "id": 1, "title": "Favorites", "system": 1},
+	{ "id": 2, "title": "Music for programming", "system": 0},
+	{ "id": 3, "title": "Driving", "system": 0},
+	{ "id": 5, "title": "Fox house", "system": 0},
+    ]
+    console.log(playlists);
+    res.send(playlists);
+});
+
+app.get('/tracks', function(req, res) {
+    var tracks= [
+	{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
+	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }
+    ]
+    console.log(tracks);
+    res.send(tracks);
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/music.html');
+});
+
+
+app.listen(3000, function () {
+    console.log('server running');
+});
