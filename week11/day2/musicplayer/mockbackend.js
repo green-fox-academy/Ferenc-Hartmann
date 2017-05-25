0e01ddd2a9cccb8e4cc179c0c40335aa5675bd60
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-05-25 13:10:29 +0200

    Foxplayer: one tracklist can be played, shuffle works too

--- week11/day2/musicplayer/mockbackend.js
+++ week11/day2/musicplayer/mockbackend.js
@@ -21,8 +21,15 @@ app.get('/playlists', function(req, res) {
 
 app.get('/tracks', function(req, res) {
     var tracks= [
-	{ "id": 21, "title": "Drift", "artist": "Untitled artist", "duration": 208, "path": "style/drift.mp3" },
-	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 208, "path": "style/drift2.mp3" }
+	{ "id": 1, "title": "Drift", "artist": "Untitled artist", "duration": 209, "path": "style/drift.mp3" },
+
+    { "id": 2, "title": "AK47", "artist": "Pisti", "duration": 2, "path": "style/AK47.mp3" },
+    { "id": 3, "title": "Double Barrel Shotgun Firing", "artist": "Jozsó", "duration": 2, "path": "style/Double Barrel Shotgun Firing.mp3" },
+    { "id": 4, "title": "Grenade", "artist": "Marcsi", "duration": 2, "path": "style/Grenade.mp3" },
+    { "id": 5, "title": "M1 Garand Single", "artist": "Erős Vulkán", "duration": 2, "path": "style/M1 Garand Single.mp3" },
+    { "id": 6, "title": "Swords Clashing", "artist": "Artúr", "duration": 2, "path": "style/Swords Clashing.mp3" },
+    { "id": 7, "title": "Tank firing", "artist": "Tankerman", "duration": 2, "path": "style/Tank firing.mp3" },
+    { "id": 8, "title": "Uzi Shooting", "artist": "GTA 5", "duration": 2, "path": "style/Uzi Shooting.mp3" },
     ]
     res.send(tracks);
 });
