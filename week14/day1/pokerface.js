efb52bfaf01362cc1e51dbb3e40de163802ca23a
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-06-12 17:17:54 +0200

    Royal flush checker function coded

--- week14/day1/pokerface.js
+++ week14/day1/pokerface.js
@@ -5,12 +5,10 @@ var PokerFace = (function() {
   function init(firstList, secondList) {
     transformer(firstList, PokerFace.leftHand);
     transformer(secondList, PokerFace.rightHand);
-    // straightFlush(PokerFace.leftHand);
-    // straightFlush(PokerFace.rightHand);
-    console.log(PokerFace.leftHand);
-    console.log(PokerFace.rightHand);
-    // console.log(straightFlush(PokerFace.leftHand));
-    // console.log(straightFlush(PokerFace.rightHand));
+    PokerFace.leftHandValue = straightFlush(PokerFace.leftHand);
+    PokerFace.rightHandValue = straightFlush(PokerFace.rightHand);
+    console.log(PokerFace.leftHandValue);
+    console.log(PokerFace.rightHandValue);
   };
 
   function transformer(list, hand) {
@@ -40,23 +38,38 @@ var PokerFace = (function() {
     };
 
     function straightFlush(hand) {
-      for (let i = 0; i < hand.length - 1; i++) {
-          if (hand[i].cardColor === hand[i+1].cardColor){
-            if (){
-            }
-          }
-      }
+      let checker = 0;
 
-        return
-      else fourofakind()
+      for (let i = 0; i < hand.length - 1; i++) {
+        if (hand[i].cardColor === hand[i+1].cardColor) {
+          if (hand[i].cardNumber == (hand[i+1].cardNumber - 1)) {
+            checker++;
+          }
+        }
       };
+
+      if (checker === hand.length - 1) {
+        return 9;
+      } else {
+        return forofaKind(hand);
+      }
+    };
+
+    function forofaKind(hand) {
+      let checker = 0;
+
+      return 8;
+    }
 
     return {
         init,
         leftHand: [],
-        rightHand: []
+        rightHand: [],
+        leftHandValue: 0,
+        rightHandValue: 0
     }
 
 })();
 
-PokerFace.init(['2H', '3D', '5S', '9C', 'KD'],['2C', '3H', '4S', '8C', 'AH'])
+// PokerFace.init(['2H', '3D', '5S', '9C', 'KD'],['2C', '3H', '4S', '8C', 'AH'])
+PokerFace.init(['2H', '3H', '4H', '5H', '6H'],['2C', '3H', '4S', '8C', 'AH'])
