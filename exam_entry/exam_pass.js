00f14bae7221a0b9f395e1e669bd076a1fdb7cc3
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-07-10 09:51:53 +0200

    main style features added

--- exam_entry/exam_pass.js
+++ exam_entry/exam_pass.js
@@ -4,16 +4,16 @@
 
 const palindrome = 'abBba';
 
-const palindromeDetector = function(string) {
+const palindromeDetector = function isPalindrome(string) {
   let reverse = '';
-  for (let i = (string.length - 1); i >= 0; i--) {
+  for (let i = (string.length - 1); i >= 0; i-= 1) {
     reverse += string[i];
   }
   if (reverse === string) {
-    return 'This string is a palindrome';
+    return 'This string is a palindrome.';
   }
   else {
-    return 'This string is not a palindrome';
+    return 'This string is not a palindrome.';
   }
 }
 
