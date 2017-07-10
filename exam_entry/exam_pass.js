e7f8091ec136e41a767774dcdf18af172b071c66
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-07-10 09:29:10 +0200

    basic code created. Styling still needed

--- exam_entry/exam_pass.js
+++ exam_entry/exam_pass.js
@@ -1,1 +1,20 @@
 'use strict'
+
+// Create a function that determines if a string is a palindrome
+
+const palindrome = 'abBba';
+
+const palindromeDetector = function(string) {
+  let reverse = '';
+  for (let i = (string.length - 1); i >= 0; i--) {
+    reverse += string[i];
+  }
+  if (reverse === string) {
+    return 'This string is a palindrome';
+  }
+  else {
+    return 'This string is not a palindrome';
+  }
+}
+
+console.log(palindromeDetector(palindrome));
