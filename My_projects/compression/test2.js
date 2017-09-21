d6b6b96dac1ac60c713ad21dc1968d9737d43299
Ferenc Hartmann <ferenc.hartmann@gmail.com>
2017-09-21 23:30:43 +0200

    bytes can be read in test2.js but need a buffer

--- My_projects/compression/test2.js
+++ My_projects/compression/test2.js
@@ -8,14 +8,11 @@ function fileRead() {
   let inputData = '';
   startTimeStamp = new Date();
 
-  fs.readFile(fileName, 'utf-8', function(err, data) {
+  fs.readFile(fileName, 'binary', function(err, data) {
     if (err) throw err;
-    for (let i = 0; i < data.length; i++) {
-      inputData = data;
-    }
     endTimeStamp = new Date();
     console.log('fileRead function duration: ' + (endTimeStamp.getTime() - startTimeStamp.getTime()) + ' msec');
-    console.log(data);
+    console.log(data.length);
   });
 }
 fileRead()
